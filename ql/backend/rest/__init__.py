ec985987926370995120a521f6cbdc9980b2b199
Christian Scholz <mrtopf@gmail.com>
2011-01-14 20:02:35 +0100

    we now use / instead of ; as we assume that we do single objects. Paths might have a different syntax later on

--- ql/backend/rest/__init__.py
+++ ql/backend/rest/__init__.py
@@ -4,6 +4,6 @@ import templates
 def setup_handlers(map):
     """setup the handlers"""
     with map.submapper(path_prefix="/1/content") as m:
-        m.connect(None, '/{content_id}{.format};{method}', handler=api.Method)
-        m.connect(None, '/{content_id}{.format}', handler=api.Item)
+        m.connect(None, '/{content_id}/{method}{.format}', handler=api.Method)
+        m.connect(None, '/{content_id}{.format}', handler=api.Method)
 
