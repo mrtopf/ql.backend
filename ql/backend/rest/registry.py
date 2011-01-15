c248e4a981f86622ee21be7a1801783c15509e82
Christian Scholz <mrtopf@gmail.com>
2011-01-15 12:16:19 +0100

    removed unused stuff and renamed type_registry to representations as this is what it is

--- ql/backend/rest/registry.py
+++ ql/backend/rest/registry.py
@@ -1,7 +1,16 @@
 import common
-import poll
-import reprs
 
+"""
+
+This module defines a mapping from types to possible representations. Usually
+a representation is invoked by appending it to a path: ``/<objid>/<representation>``,
+e.g. ``/2762862/children``.
+
+"""
+
+# this is a mapping from representation name to representation handler
+# this is the common mapping for all base types and should be supported
+# by all addon typed. They can extend this as they wish of course.
 r_common = {
         'subtree' : common.SubTree,
         'query' : common.Query,
@@ -11,11 +20,11 @@ r_common = {
         'post' : common.Post,
 }
 
-type_registry = {
+# this is the registry mapping type names to the representation mapping
+representations = {
     'status' : r_common,
     'link' : r_common,
     'folder' : r_common,
-     
 }
 
-__all__ = ['type_registry']
+__all__ = ['representations']
